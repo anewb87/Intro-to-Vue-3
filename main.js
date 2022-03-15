5e494edfa44f9a703f9907c0b07925d20d0a1348
Alexis Newby <alexis.newby@gmail.com>
2022-03-15 09:19:16 -0600

    L3

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.amazon.com/CHAMPRO-Sports-Multi-Sport-Socks-Medium/dp/B01MY98TEW/ref=sr_1_5?crid=3U855N8DZ5L2X&keywords=blue+socks&qid=1647313849&sprefix=blue+socks%2Caps%2C149&sr=8-5'
         }
     }
 })
